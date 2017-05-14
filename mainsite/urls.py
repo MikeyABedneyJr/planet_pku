0e0bb8d17edc11ba4d1c6919293584f10ef81de7
mikeyabedneyjr <mike.bedney@gmail.com>
2017-05-14 15:22:31 -0700

    starting up a new wave of work

--- mainsite/urls.py
+++ mainsite/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
+# Todo: add other views - about_pku is template
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about_pku', views.about_pku, name='about_pku'),
