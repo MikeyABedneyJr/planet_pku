adef2983006b0b5fb5f965bd6fa6232ae88ec5b2
mikeyabedneyjr <mike.bedney@gmail.com>
2017-06-07 12:02:21 -0400

    navbar set except logo

--- mainsite/urls.py
+++ mainsite/urls.py
@@ -5,5 +5,12 @@ from mainsite import views
 # Todo: add other views - about_pku is template
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^about_us', views.about_us, name='about_us'),
     url(r'^about_pku', views.about_pku, name='about_pku'),
+    url(r'^community_forums', views.community_forums, name='community_forums'),
+    url(r'^my_account', views.my_account, name='my_account'),
+    url(r'^private_policy', views.private_policy, name='private_policy'),
+    url(r'^submit_recipe', views.submit_recipe, name='submit_recipe'),
+    url(r'^user_agreement', views.user_agreement, name='user_agreement'),
+    url(r'^view_recipe', views.view_recipe, name='view_recipe'),
 ]
--- mainsite/views.py
+++ mainsite/views.py
@@ -8,5 +8,26 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
+def about_us(request):
+    return render(request, 'about_us.html')
+
 def about_pku(request):
     return render(request, 'about_pku.html')
+
+def community_forums(request):
+    return render(request, 'community_forums.html')
+
+def my_account(request):
+    return render(request, 'my_account.html')
+
+def private_policy(request):
+    return render(request, 'private_policy.html')
+
+def submit_recipe(request):
+    return render(request, 'submit_recipe.html')
+
+def user_agreement(request):
+    return render(request, 'user_agreement.html')
+
+def view_recipe(request):
+    return render(request, 'view_recipe.html')
